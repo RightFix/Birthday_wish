fea430144e88863e34afdfb1d54c2207b4618070
Right Fix <righteousnessude@gmail.com>
2025-08-12 23:44:40 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,10 +7,10 @@ st.title(f"🥳 Happy Birthday Best 🎈")
 wishes = ["Happy Birthday, My Best! 🎉💖", "Ogunnike Motorola Oyindamola", "You’re more than a friend to me", "Thank you for filling my life with laughter, love, and endless memories.",  "I’m so grateful for every moment we’ve shared and excited for all the adventures ahead." , "May this year bring you joy as bright as your smile" , "Blessings as countless as your dreams", "and love that surrounds you always." , "You deserve nothing less than the absolute best today and forever. 💐✨"]
 
 st.audio("Simi-Happy-Birthday.mp3", loop=True, autoplay=True, width=2)
-def wish():
+def wishing():
   for wish in wishes:
           st.markdown(f"<p style='text-align: center;'>{wish}</p>", unsafe_allow_html=True)
           time.sleep(2)
-          wish()
-wish()
+          wishing()
+wishing()
 #st.rerun()
