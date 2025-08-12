c4419d4ac2b42f3cea9e87c5e1a5a52048bbed34
Right Fix <righteousnessude@gmail.com>
2025-08-12 23:07:53 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,16 +1,17 @@
 import streamlit as st
 import time
 
-st.title("🥳🎂 Happy Birthday Motorola")
+st.title("🥳🎂 Happy Birthday 🎈🎂")
 
-audio_url = "'https://open.spotify.com/track/0HRVRl4bHTe0fkng5HAxpg?si=AjCOoN-yQCKOFo45kLvczw%0A'"  # Replace with your link
+audio_url = ”Simi-Happy-Birthday.mp3”# Replace with your link
 
 st.markdown( f"<audio autoplay loop style='display:none'> <source src={audio_url} type='audio/mp3'> </audio> ", unsafe_allow_html=True)
 st.audio("https://open.spotify.com/track/0HRVRl4bHTe0fkng5HAxpg?si=AjCOoN-yQCKOFo45kLvczw%0A", autoplay= True, loop= True)
-wishes = ["Happy Birthday, My Best! 🎉💖", "Ogunnike Motorola Oyindamola", "You’re more than a friend  you’re family to me", "Thank you for filling my life with laughter, love, and endless memories.",  "I’m so grateful for every moment we’ve shared and excited for all the adventures ahead." , "May this year bring you joy as bright as your smile" , "blessings as countless as your dreams", "and love that surrounds you always." , "You deserve nothing less than the absolute best today and forever. 💐✨"]
+
+wishes = ["Happy Birthday, My Best! 🎉💖", "Ogunnike Motorola Oyindamola", "You’re more than a friend to me", "Thank you for filling my life with laughter, love, and endless memories.",  "I’m so grateful for every moment we’ve shared and excited for all the adventures ahead." , "May this year bring you joy as bright as your smile" , "Blessings as countless as your dreams", "and love that surrounds you always." , "You deserve nothing less than the absolute best today and forever. 💐✨"]
 
 for wish in wishes:
      st.markdown(f"<p style='text-align: center;'>{wish}</p>", unsafe_allow_html=True)
-     time.sleep(3)
+     time.sleep(2)
 
 st.rerun()
