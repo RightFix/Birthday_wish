3d910138f3696644c422462bf9715eb2f433f0f2
Right Fix <righteousnessude@gmail.com>
2025-08-12 18:36:57 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,14 @@ st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", loop= True, autoplay= True)
+
+
+if 'show_widget' not in st.session_state:
+    st.session_state.show_widget = True
+
+st.checkbox("Toggle Widget Visibility", value=st.session_state.show_widget, key="toggle_visibility")
+
+if st.session_state.toggle_visibility:
+    st.text_input("This widget is visible", "Hello", key="visible_input")
+else:
+    st.write("Widget is hidden.")
