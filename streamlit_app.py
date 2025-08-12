4159fb14f6a41dfc3133f0985f445ad51feed6b2
Right Fix <righteousnessude@gmail.com>
2025-08-12 23:56:20 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,7 @@ import streamlit as st
 import time
 
 st.title(f"🥳 Happy Birthday Best 🎈")
-st.audio("Simi-Happy-Birthday.mp3", loop=True, autoplay=True, width=4)
+st.audio("Simi-Happy-Birthday.mp3", loop=True, autoplay=True, width=10)
 
 wishes = ["Happy Birthday, My Best! 🎉💖", "Ogunnike Motorola Oyindamola", "You’re more than a friend to me", "Thank you for filling my life with laughter, love, and endless memories.",  "I’m so grateful for every moment we’ve shared and excited for all the adventures ahead." , "May this year bring you joy as bright as your smile" , "Blessings as countless as your dreams", "and love that surrounds you always." , "You deserve nothing less than the absolute best today and forever. 💐✨"]
 
