6d1efaa9aa03c4148013e69ca946ab801a6623a5
Right Fix <righteousnessude@gmail.com>
2025-08-12 21:48:11 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,5 @@ wishes = ["Happy Birthday, My Best! 🎉💖", "Ogunnike Motorola Oyindamola",
 
 
 for wish in wishes:
-  col1, col2, col3 = st.columns([1, 2, 1])
-  with col2:
-    st.write(wish)
-    time.sleep(3)
+  st.markdown(f"<p style='text-align: center;'>{wish}</p>", unsafe_allow_html=True)
+  time.sleep(3)
