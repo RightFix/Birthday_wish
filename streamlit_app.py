d319510e2f228bce5169ba96fe05cfb68e12c281
Right Fix <righteousnessude@gmail.com>
2025-08-12 21:42:46 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -8,6 +8,9 @@ st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", loop= 
 wishes = ["Happy Birthday, My Best! 🎉💖", "Ogunnike Motorola Oyindamola", 
 "You’re more than a friend  you’re family to me", "Thank you for filling my life with laughter, love, and endless memories.",  "I’m so grateful for every moment we’ve shared and excited for all the adventures ahead." , "May this year bring you joy as bright as your smile" , "blessings as countless as your dreams", "and love that surrounds you always." , "You deserve nothing less than the absolute best today and forever. 💐✨"]
 
+
 for wish in wishes:
-  st.write(wish)
-  time.sleep(3)
+  col1, col2, col3 = st.columns([1, 2, 1])
+  with col2:
+    st.write(wish)
+    time.sleep(3)
