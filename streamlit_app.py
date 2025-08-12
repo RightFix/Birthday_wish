2d8d325ccb975f2b63095022c5ef7177b74cc596
Right Fix <righteousnessude@gmail.com>
2025-08-12 23:32:16 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,12 +3,15 @@ import time
 
 st.title("🥳 Happy Birthday 🎈")
 
-st.audio("Simi-Happy-Birthday.mp3", loop=True, autoplay=True )
 
 wishes = ["Happy Birthday, My Best! 🎉💖", "Ogunnike Motorola Oyindamola", "You’re more than a friend to me", "Thank you for filling my life with laughter, love, and endless memories.",  "I’m so grateful for every moment we’ve shared and excited for all the adventures ahead." , "May this year bring you joy as bright as your smile" , "Blessings as countless as your dreams", "and love that surrounds you always." , "You deserve nothing less than the absolute best today and forever. 💐✨"]
 
-for wish in wishes:
+if st.button("click me"):
+  
+  st.audio("Simi-Happy-Birthday.mp3", loop=True, autoplay=True )
+
+  for wish in wishes:
      st.markdown(f"<p style='text-align: center;'>{wish}</p>", unsafe_allow_html=True)
      time.sleep(2)
 
-st.rerun()
+  st.rerun()
