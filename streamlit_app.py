0a6d48ac6b7130634ab2a97ce18eba3a77e81094
Right Fix <righteousnessude@gmail.com>
2025-08-12 21:16:13 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,13 +2,11 @@ import streamlit as st
 import time
 
 st.title("🥳🎂 Happy Birthday Motorola")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
 
 st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", loop= True, autoplay= True)
 
-wishes = []
+wishes = ["Happy Birthday, My Best! 🎉💖", "Ogunnike Motorola Oyindamola", 
+"You’re more than a friend  you’re family to me", "Thank you for filling my life with laughter, love, and endless memories.",  "I’m so grateful for every moment we’ve shared and excited for all the adventures ahead." , "May this year bring you joy as bright as your smile" , "blessings as countless as your dreams", "and love that surrounds you always." , "You deserve nothing less than the absolute best today and forever. 💐✨"]
 
 for wish in wishes:
   st.write(wish)
