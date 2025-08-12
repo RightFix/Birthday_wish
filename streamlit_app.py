6f3fd0ad852b6821bab00fabcb25ab3c154e6d6d
Right Fix <righteousnessude@gmail.com>
2025-08-12 21:09:53 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,18 +1,15 @@
 import streamlit as st
+import time
 
 st.title("🥳🎂 Happy Birthday Motorola")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
+st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", loop= True, autoplay= True)
 
-if 'show_widget' not in st.session_state:
-    st.session_state.show_widget = True
+wishes = []
 
-st.checkbox("Toggle Widget Visibility", value=st.session_state.show_widget, key="toggle_visibility")
-
-if st.session_state.toggle_visibility:
-    st.text_input("This widget is visible", "Hello", key="visible_input")
-    st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3", loop= True, autoplay= True)
-else:
-    st.write("Widget is hidden.")
+for wish in wishes:
+  st.write(wish)
+  time.sleep(3)
