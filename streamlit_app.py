c874addeebc20c85f9532a42e8328ed347e0e422
Right Fix <righteousnessude@gmail.com>
2025-08-12 18:22:35 +0100

    update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,7 @@
 import streamlit as st
 
-st.title("🎈 My new app")
+st.title("🥳🎂 Happy Birthday Motorola")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
+st.audio("https://www.soundhelix.com/examples/mp3/SoundHelix-Song-1.mp3")
